c0a234ab95faece5559dbe8061f8f36bcf16073e
Gonzalo Varisco <gonzalojvarisco@gmail.com>
2018-09-16 22:30:18 -0300

    avances acceso datos y referencias

---
+++ Negocio/AccesoDatos.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace Negocio
+{
+    public class AccesoDatos
+    {
+        private SqlConnection conexion;
+        private SqlCommand  comando;
+        private SqlDataReader lector;
+
+    }
+}
